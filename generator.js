423ead69add2564b0c07e0475e1159bc478e6676
Bhaskar Tiwari <bt@qbuzzer.com>
2014-10-13 03:48:23 +0530

    Version 2

---
+++ generator.js
@@ -0,0 +1,52 @@
+var fs=require("fs"),
+svg2ttf=require("svg2ttf"),
+md5=require("MD5")
+fs.readFile(__dirname+"/static/font.svg","utf8",function(e,d){
+	var char1=["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+	charRand=[],
+	send={}
+	shuffle(char1)
+	char1.forEach(function(c,i){
+		charRand.push("&#"+(200+i)+";")
+	})
+	var char2=char1.splice(0,31)
+	send.seed=md5(rand(25,50))
+	fs.exists(__dirname+"/static/gen/font_"+send.seed+".map",function(e){
+		if(e){
+			send.map=JSON.parse(fs.readFileSync(__dirname+"/static/gen/font_"+send.seed+".map"))
+			process.send(send)
+			process.exit(1)
+		}
+		send.map={}
+		char1.forEach(function(c,i){
+			send.map[c]=charRand.shift()
+			var regex=new RegExp("\""+c+"\"","g")
+			d=d.replace(regex,"\""+send.map[c]+"\"")
+		})
+		var ttf=svg2ttf(d)
+		fs.writeFileSync(__dirname+"/static/gen/font_a_"+send.seed+".ttf",new Buffer(ttf.buffer))
+		d=fs.readFileSync(__dirname+"/static/font.svg","utf8")
+		char2.forEach(function(c,i){
+			send.map[c]=charRand.shift()
+			var regex=new RegExp("\""+c+"\"","g")
+			d=d.replace(regex,"\""+send.map[c]+"\"")
+		})
+		var ttf=svg2ttf(d)
+		fs.writeFileSync(__dirname+"/static/gen/font_b_"+send.seed+".ttf",new Buffer(ttf.buffer))
+		fs.writeFileSync(__dirname+"/static/gen/font_"+send.seed+".map",JSON.stringify(send.map))
+		process.send(send)
+	})
+})
+function rand(l,h){
+    return Math.floor(Math.random()*(h-l+1)+l)
+}
+function shuffle(a){
+	var array=a,currentIndex=array.length,temporaryValue,randomIndex
+	while (0!==currentIndex){
+		randomIndex=Math.floor(Math.random()*currentIndex)
+		currentIndex-=1
+		temporaryValue=array[currentIndex]
+		array[currentIndex]=array[randomIndex]
+		array[randomIndex]=temporaryValue
+	}
+}
---
+++ index.js
@@ -0,0 +1,35 @@
+var express=require("express"),
+app=new express(),
+cp=require("child_process"),
+fs=require("fs"),
+opt
+app.listen(process.env.PORT || 3000,function(){
+	app.use("/static",express.static(__dirname+"/static")).use(function(req,res){
+		fs.readFile(__dirname+"/index.htm","utf8",function(e,d){
+			d=d.replace(/%SEED%/g,opt.seed)
+			d=protext(d,"<h1>","</h1>")
+			d=protext(d,"<h2>","</h2>")
+			res.send(d)
+		})
+	})
+})
+var generator=function(){ 
+	cp.fork(__dirname+"/generator.js").on("message",function(m){
+		opt=m
+		setTimeout(generator,5000)
+	})
+}
+var protext=function(d,tag1,tag2){
+	var rx=new RegExp(tag1+"(.*?)"+tag2)
+	var m=d.match(rx)
+	var m=m[1].split("")
+	m.forEach(function(c,i){
+		if(opt.map[c]){
+			m[i]=opt.map[c]
+		}
+	})
+	rx=new RegExp(tag1+".*?"+tag2)
+	return d.replace(rx,tag1+m.join("")+tag2)
+}
+generator()
+
